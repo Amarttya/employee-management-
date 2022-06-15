256a98a6aaae372aadb321542425b67c82610cb0
Amarttya <amarttya.prasad.dey@g.bracu.ac.bd>
2022-06-15 13:33:54 +0600

    well done

---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+
+const app = express();
+
+const mysql = require("mysql");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "Adi01521307723",
+  database: "employeesystem",
+  //port: 3306,
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name , age , country , position , wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("value Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete", (req, res) => {
+  //const deleteName = req.body.name;
+
+  db.query(
+    `DELETE FROM employees WHERE name = ? `, [req.body.name],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.listen(3001, () => console.log(`server is running on ${3001}`));
